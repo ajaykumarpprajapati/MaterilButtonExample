2905938b1f8a7a5f1538be6a797f8ffd2196e069
Ajay Prajapati <ajay4android@gmail.com>
2019-06-03 16:26:49 +0530

    elevated button added

--- app/src/main/java/india/ajay/materilbuttonexample/MainActivity.kt
+++ app/src/main/java/india/ajay/materilbuttonexample/MainActivity.kt
@@ -2,11 +2,22 @@ package india.ajay.materilbuttonexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var elevatedButton: AppCompatButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+    }
+
+    private fun setupView(){
+        elevatedButton = findViewById(R.id.elevated_button)
+        elevatedButton?.setOnClickListener {
+
+        }
     }
 }
